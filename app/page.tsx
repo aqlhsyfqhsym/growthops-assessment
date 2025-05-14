32fb6256301351639819a9d1566f518d03bb41a0
aqlhsyfqhsym <nuraqilah.syafiqah@yahoo.com>
2025-05-14 15:50:29 +0800

    create base

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import ScrollingClient from "@/components/ScrollingClient";
+
+export default function Home() {
+  return (
+    <div className="scroll-smooth items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          src="/landing.png"
+          alt="intro page"
+          fill
+          className="object-cover"
+        />
+        <ScrollingClient>
+          <div>Your custom content</div>
+        </ScrollingClient> 
+      </main>
+    </div>
+  );
+}
